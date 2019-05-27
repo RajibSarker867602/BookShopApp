42470137333bdbeaa67e9da4d4a847e4315531c2
RajibSarker867602 <RajibSarker3202@gmail.com>
2019-05-27 09:59:00 +0600

    ModifyBookShopAppUsingList_Added

--- BookShopApp/BookShopApp/BookShopEntryForm.cs
+++ BookShopApp/BookShopApp/BookShopEntryForm.cs
@@ -16,26 +16,25 @@ namespace BookShopApp
         {
             InitializeComponent();
         }
-        const int size = 100;
-        int index = 0;
-        string[] name = new string[size];
-        string[] contactNo = new string[size];
-        string[] address = new string[size];
-        string[] order = new string[size];
-        int[] quantity = new int[size];
-        decimal[] totalPrice = new decimal[size];
+        List<string> names = new List<string>();
+        List<string> contacts = new List<string>();
+        List<string> addresses = new List<string>();
+        List<string> orders = new List<string>();
+        List<int> quantites = new List<int>();
+        List<decimal> totalPrices = new List<decimal>();
 
 
         private void SaveButton_Click(object sender, EventArgs e)
         {
             try
             {
+                int index = 0;
                 decimal price = 0;
                 string message = "";
-                name[index] = nameTextBox.Text;
-                contactNo[index] = contactNoTextBox.Text;
-                address[index] = addressTextBox.Text;
-                order[index] = orderComboBox.Text;
+                names.Add(nameTextBox.Text);
+                contacts.Add(contactNoTextBox.Text);
+                addresses.Add(addressTextBox.Text);
+                orders.Add(orderComboBox.Text);
                 if (orderComboBox.Text == "Math")
                 {
                     price = 120;
@@ -52,16 +51,17 @@ namespace BookShopApp
                 {
                     price = 80;
                 }
-                quantity[index] = Convert.ToInt32(quantityTextBox.Text);
-                decimal subTotalPrice = quantity[index] * price;
-                totalPrice[index] = subTotalPrice;
-                for (int index = 0; index < size; index++)
+                quantites.Add(Convert.ToInt32(quantityTextBox.Text));
+                decimal subTotalPrice = quantites[index] * price;
+                totalPrices.Add(subTotalPrice);
+                foreach (decimal totalPrice in totalPrices)
                 {
-                    if (name[index] != "" && name[index] != null)
-                        message = message + "\t\tShow Customer " + (index + 1) + " Information" + Environment.NewLine + "\t=============================" + Environment.NewLine + Environment.NewLine + "Name : " + name[index] + Environment.NewLine + "Contact No : " + contactNo[index] + Environment.NewLine + "Address : " + address[index] + Environment.NewLine + "Your ordered item : " + order[index] + Environment.NewLine + "Quantity : " + quantity[index] + Environment.NewLine + "Your total price : " + totalPrice[index] + Environment.NewLine + Environment.NewLine + "\t Thank you for stay with us...\n\n";
+                    //if (name[index] != "" && name[index] != null)
+                    message = message + "\t\tShow Customer " + (index + 1) + " Information" + Environment.NewLine + "\t=============================" + Environment.NewLine + Environment.NewLine + "Name : " + names[index] + Environment.NewLine + "Contact No : " + contacts[index] + Environment.NewLine + "Address : " + addresses[index] + Environment.NewLine + "Your ordered item : " + orders[index] + Environment.NewLine + "Quantity : " + quantites[index] + Environment.NewLine + "Your total price : " + totalPrice + Environment.NewLine + Environment.NewLine + "\t Thank you for stay with us...\n\n";
+                    index++;
                 }
                 showRichTextBox.Text = message;
-                index++;
+
                 nameTextBox.Text = "";
                 contactNoTextBox.Text = "";
                 addressTextBox.Text = "";
